497911f6021b70094fe36b2750159edd41405a4c
깃ChertHyeon <r4560798@naver.com>
2023-05-07 21:12:26 +0900

    canModify 수정

--- src/main/java/com/ll/gramgram/boundedContext/likeablePerson/service/LikeablePersonService.java
+++ src/main/java/com/ll/gramgram/boundedContext/likeablePerson/service/LikeablePersonService.java
@@ -167,7 +167,7 @@ public class LikeablePersonService {
 
     @Transactional
     public RsData<LikeablePerson> modifyAttractive(Member actor, LikeablePerson likeablePerson, int attractiveTypeCode) {
-        RsData canModifyRsData = canModifyLike(actor, likeablePerson);
+        RsData canModifyRsData = canModify(actor, likeablePerson);
 
         if (canModifyRsData.isFail()) {
             return canModifyRsData;
@@ -210,7 +210,7 @@ public class LikeablePersonService {
         }
     }
 
-    public RsData canModifyLike(Member actor, LikeablePerson likeablePerson) {
+    public RsData canModify(Member actor, LikeablePerson likeablePerson) {
         if (!actor.hasConnectedInstaMember()) {
             return RsData.of("F-1", "먼저 본인의 인스타그램 아이디를 입력해주세요.");
         }
@@ -218,13 +218,13 @@ public class LikeablePersonService {
         InstaMember fromInstaMember = actor.getInstaMember();
 
         if (!Objects.equals(likeablePerson.getFromInstaMember().getId(), fromInstaMember.getId())) {
-            return RsData.of("F-2", "해당 호감표시를 수정할 권한이 없습니다.");
+            return RsData.of("F-2", "해당 호감표시에 대해 사유변경을 수행할 권한이 없습니다.");
         }
 
         if(!likeablePerson.isModifyUnlocked()){
-            return RsData.of("F-8", "호감 표시 후 3시간 이후 호감 사유 수정이 가능합니다.");
+            return RsData.of("F-3", "아직 호감사유변경을 할 수 없습니다. %s 후에는 가능합니다.".formatted(likeablePerson.getModifyUnlockDateRemainStrHuman()));
         }
 
-        return RsData.of("S-1", "호감 사유 수정이 가능합니다.");
+        return RsData.of("S-1", "호감사유변경이 가능합니다.");
     }
 }
