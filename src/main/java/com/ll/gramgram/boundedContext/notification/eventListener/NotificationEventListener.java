8a62bf7015cc602dc30ed55043e9ef5b65cf41b8
깃ChertHyeon <r4560798@naver.com>
2023-05-07 22:09:21 +0900

    호감 표시, 사유 변경 시 알람 객체 생성(수정)

--- src/main/java/com/ll/gramgram/boundedContext/notification/eventListener/NotificationEventListener.java
+++ src/main/java/com/ll/gramgram/boundedContext/notification/eventListener/NotificationEventListener.java
@@ -2,6 +2,7 @@ package com.ll.gramgram.boundedContext.notification.eventListener;
 
 import com.ll.gramgram.base.event.*;
 import com.ll.gramgram.boundedContext.instaMember.service.InstaMemberService;
+import com.ll.gramgram.boundedContext.likeablePerson.entity.LikeablePerson;
 import com.ll.gramgram.boundedContext.notification.service.NotificationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
@@ -10,26 +11,31 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @RequiredArgsConstructor
+@Transactional
 public class NotificationEventListener {
     private final NotificationService notificationService;
 
     // 호감사유 수정했을 때 알림 객체 생성하는 이벤트 리스너
     @EventListener
-    @Transactional
     public void listen(EventAfterModifyAttractiveType event) {
-        notificationService.whenAfterModifyAttractiveType(event.getLikeablePerson(), event.getOldAttractiveTypeCode());
+
+        LikeablePerson likeablePerson = event.getLikeablePerson();
+        notificationService.makeModifyAttractive(likeablePerson, event.getOldAttractiveTypeCode());
+        // 수정 전
+        // notificationService.whenAfterModifyAttractiveType(event.getLikeablePerson(), event.getOldAttractiveTypeCode());
     }
 
     // 호감표시 했을 때 알림 객체 생성하는 이벤트 리스너
     @EventListener
-    @Transactional
     public void listen(EventAfterLike event) {
-        notificationService.whenAfterLike(event.getLikeablePerson());
+        LikeablePerson likeablePerson = event.getLikeablePerson();
+        notificationService.makeLike(likeablePerson);
+        // 수정 전
+        // notificationService.whenAfterLike(event.getLikeablePerson());
     }
 
     // 사용자가 알림창 클릭했을때 발생하는 이벤트(readDate 업데이트) 리스너
     @EventListener
-    @Transactional
     public void listen(EventClickNotification event) {
         notificationService.whenClickNotification(event.getNotifications());
     }
--- src/main/java/com/ll/gramgram/boundedContext/notification/service/NotificationService.java
+++ src/main/java/com/ll/gramgram/boundedContext/notification/service/NotificationService.java
@@ -1,18 +1,21 @@
 package com.ll.gramgram.boundedContext.notification.service;
 
 
+import com.ll.gramgram.base.rsData.RsData;
 import com.ll.gramgram.boundedContext.instaMember.entity.InstaMember;
 import com.ll.gramgram.boundedContext.likeablePerson.entity.LikeablePerson;
 import com.ll.gramgram.boundedContext.notification.entity.Notification;
 import com.ll.gramgram.boundedContext.notification.repository.NotificationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class NotificationService {
     private final NotificationRepository notificationRepository;
 
@@ -20,6 +23,7 @@ public class NotificationService {
         return notificationRepository.findByToInstaMember(toInstaMember);
     }
 
+    @Transactional
     // 호감 사유 수정 시 알림 객체 생성 메서드
     public void whenAfterModifyAttractiveType(LikeablePerson likeablePerson, int oldAttractiveTypeCode) {
         Notification modifyMsgNotification = Notification
@@ -34,6 +38,7 @@ public class NotificationService {
         notificationRepository.save(modifyMsgNotification);
     }
 
+    @Transactional
     // 호감 표시 알림 객체 생성 메서드
     public void whenAfterLike(LikeablePerson likeablePerson) {
 
@@ -49,6 +54,7 @@ public class NotificationService {
     }
 
     // 알림 클릭 시 readDate 업데이트 메서드
+    @Transactional
     public void whenClickNotification(List<Notification> notifications) {
         for (Notification notification : notifications) {
             // 조건 추가 (알람을 안읽었으면 수정하는 것, 과제 풀이 시 놓침)
@@ -62,5 +68,32 @@ public class NotificationService {
     // 테스트 사용 목적 메서드
     public Notification findById(Long id) {
         return notificationRepository.findById(id).get();
+    }
+
+    @Transactional
+    public RsData<Notification> makeLike(LikeablePerson likeablePerson) {
+        return make(likeablePerson, "LIKE", 0, null);
+    }
+
+    private RsData<Notification> make(LikeablePerson likeablePerson, String typeCode, int oldAttractiveTypeCode, String oldGender) {
+        Notification notification = Notification
+                .builder()
+                .typeCode(typeCode)
+                .toInstaMember(likeablePerson.getToInstaMember())
+                .fromInstaMember(likeablePerson.getFromInstaMember())
+                .oldAttractiveTypeCode(oldAttractiveTypeCode)
+                .oldGender(oldGender)
+                .newAttractiveTypeCode(likeablePerson.getAttractiveTypeCode())
+                .newGender(likeablePerson.getFromInstaMember().getGender())
+                .build();
+
+        notificationRepository.save(notification);
+
+        return RsData.of("S-1", "알림 메세지가 생성되었습니다.", notification);
+    }
+
+    @Transactional
+    public RsData<Notification> makeModifyAttractive(LikeablePerson likeablePerson, int oldAttractiveTypeCode) {
+       return make(likeablePerson, "MODIFY_ATTRACTIVE_TYPE", oldAttractiveTypeCode, likeablePerson.getFromInstaMember().getGender());
     }
 }
