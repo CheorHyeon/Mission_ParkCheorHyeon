75f8441ac27a3947bab1e0fdd07674966fdf7f4f
깃ChertHyeon <r4560798@naver.com>
2023-05-08 17:13:36 +0900

    미션 준비

--- src/main/java/com/ll/gramgram/base/rq/Rq.java
+++ src/main/java/com/ll/gramgram/base/rq/Rq.java
@@ -8,15 +8,18 @@ import com.ll.gramgram.standard.util.Ut;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.context.MessageSource;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.savedrequest.SavedRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.RequestScope;
+import org.springframework.web.servlet.LocaleResolver;
 
 import java.util.Date;
-
+import java.util.Locale;
+import java.util.Map;
 @Component
 @RequestScope
 public class Rq {
@@ -27,14 +30,20 @@ public class Rq {
     private final User user;
     private Member member = null; // 레이지 로딩, 처음부터 넣지 않고, 요청이 들어올 때 넣는다.
 
+    private final MessageSource messageSource;
+    private final LocaleResolver localeResolver;
+    private Locale locale;
+
     private final NotificationService notificationService;
 
-    public Rq(MemberService memberService, HttpServletRequest req, HttpServletResponse resp, HttpSession session, NotificationService notificationService) {
+    public Rq(MemberService memberService, HttpServletRequest req, HttpServletResponse resp, HttpSession session, NotificationService notificationService, MessageSource messageSource, LocaleResolver localeResolver) {
         this.memberService = memberService;
         this.req = req;
         this.resp = resp;
         this.session = session;
         this.notificationService = notificationService;
+        this.messageSource = messageSource;
+        this.localeResolver = localeResolver;
 
         // 현재 로그인한 회원의 인증정보를 가져옴
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -144,5 +153,21 @@ public class Rq {
         if (!actor.hasConnectedInstaMember()) return false;
 
         return notificationService.countUnreadNotificationsByToInstaMember(getMember().getInstaMember());
+    }
+
+    public String getCText(String code, String... args) {
+        return messageSource.getMessage(code, args, getLocale());
+    }
+
+    private Locale getLocale() {
+        if (locale == null) locale = localeResolver.resolveLocale(req);
+
+        return locale;
+    }
+
+    public String getParamsJsonStr() {
+        Map<String, String[]> parameterMap = req.getParameterMap();
+
+        return Ut.json.toStr(parameterMap);
     }
 }
